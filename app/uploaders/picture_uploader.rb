772b6bb1c6c5c79521a2858b0330f8cb11ed6c6b
alow <alowseng@gmail.com>
2016-11-17 08:10:04 +0000

    image resize

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,7 +1,7 @@
 class PictureUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
-  include CarrierWave::RMagick
+  include CarrierWave::MiniMagick
   process resize_to_limit: [400, 300] 
   # include CarrierWave::MiniMagick
 
