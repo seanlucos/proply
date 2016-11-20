331bdcbbf186b134dff2ef4d260c519ecb6ca48f
alow <alowseng@gmail.com>
2016-11-20 05:51:07 +0000

    upload test

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -16,7 +16,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     #"#{model.class.to_s.underscore}"
-    "#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "#{model.class.to_s.underscore}/#{model.id}"
    #     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
     #mm_folder1 = "images"
     #"#{mm_folder1}/#{model.class.to_s.underscore}"
