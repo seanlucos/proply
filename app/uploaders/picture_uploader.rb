e442210e1169907dfeb98e6850e7e41c93c96ed5
alow <alowseng@gmail.com>
2016-11-20 03:48:26 +0000

    upload test

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -17,7 +17,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   def store_dir
     #"#{model.class.to_s.underscore}"
     #"uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-    "#{model.id}"
+    "#{model.request_id}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
