03833219743c056ca4e91d7c6f5b6621f756a25d
Anthony Low <lowseng@yahoo.com>
2016-11-24 07:38:57 +0000

    gravatar

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -5,4 +5,13 @@ class User::RegistrationsController < Devise::RegistrationsController
     devise_parameter_sanitizer.for(:sign_up).push(:name, :telephone, :agentno, :prefercountry)
     devise_parameter_sanitizer.for(:account_update).push(:name, :telephone, :agentno, :prefercountry)
   end
+
+#did this with error. copied to erb instead 24-nov-2016
+#  def new
+#    @places = Place.where(status: true)
+#    @places_for_dropdown = []
+#    @places.each do |i|
+#      @places_for_dropdown << [i.name, i.id]
+#    end    
+#  end
 end
