c85a1b87b219c2c32113cff0862fe27fa847cce1
Anthony Low <lowseng@yahoo.com>
2016-11-30 04:02:01 +0000

    layout maint

--- app/models/article.rb
+++ app/models/article.rb
@@ -10,7 +10,7 @@ class Article < ActiveRecord::Base
   
   
  	validates :title, presence: true, length: {minimum: 3, maximum: 50}
-	validates :description, presence: true, length: {minimum: 10, maximum: 500}
+	validates :description, presence: true, length: {minimum: 10, maximum: 1000}
 	#has_many :images, dependent: :destroy
 
   # multiple image uploading
