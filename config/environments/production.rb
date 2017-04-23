fd37b264f4a9beea0bb0f4a1a0c5a11f0db50f3b
Chanson <landmarketing001@gmail.com>
2017-04-23 05:03:06 +0000

    sengrid return email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'vast-retreat-95360.herokuapp.com/', :protocol => 'https'}
+  config.action_mailer.default_url_options = { :host => 'landmark.my/', :protocol => 'https'}
 
 end
